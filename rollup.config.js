11c0a1230264e508e7d80caadf4e82fc5d9d1e9e
Darren <theinvader360@gmail.com>
2024-01-28 14:20:10 +0000

    minify fc64.js bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,25 @@
 import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
 import replace from "@rollup/plugin-replace";
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: "src/main.ts",
-  output: {
-    file: "lib/fc64.js",
-    format: "iife",
-    name: "window",
-    extend: true,
-  },
+  output: [
+    {
+      file: "lib/fc64.js",
+      format: "iife",
+      name: "window",
+      extend: true,
+    },
+    {
+			file: "lib/fc64.min.js",
+			format: "iife",
+			name: "window",
+      extend: true,
+			plugins: [terser()],
+		}
+  ],
   plugins: [
     typescript({ tsconfig: "tsconfig.json" }),
     commonjs(),
